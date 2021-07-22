eae97a0cbe29004d9cf9c8553f43360a68384cf8
huanent <huanent@icloud.com>
2021-07-22 21:59:01 +0800

    Add project files.

---
+++ src/DotnetVueSsr/RenderContext.cs
@@ -0,0 +1,61 @@
+﻿using Esprima;
+using Esprima.Ast;
+using Jint;
+using Jint.Native;
+using Jint.Native.Function;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetVueSsr
+{
+    public class RenderContext
+    {
+        static readonly Script libs = new JavaScriptParser(ScriptResource.Load()).ParseScript();
+
+        public Engine Engine { get; }
+
+        public RenderContext() : this(new Engine())
+        {
+
+        }
+
+        public RenderContext(Engine engine)
+        {
+            Engine = engine;
+            Engine.Execute(libs);
+        }
+
+
+        public void AddComponent(string name, string html, string js, string css)
+        {
+            var func = Engine.Evaluate(js) as ScriptFunctionInstance;
+            var vm = func.Call(null, Array.Empty<JsValue>());
+            vm.AsObject().Set("template", html);
+            var Vue = Engine.GetValue("Vue");
+            var vueCompoment = Vue.Get("component") as ScriptFunctionInstance;
+            vueCompoment.Call(Vue, new[] { name, vm });
+        }
+
+        public string Render(string html, string js, string css)
+        {
+            var vm = (Engine.Evaluate(js) as ScriptFunctionInstance).Call(null, Array.Empty<JsValue>());
+            vm.AsObject().Set("template", html);
+            var renderVueComponentToString = Engine.GetValue("renderVueComponentToString") as ScriptFunctionInstance;
+            var result = string.Empty;
+            var vue = Engine.Evaluate("(vm)=>new Vue(vm)") as ScriptFunctionInstance;
+            vm = vue.Call(null, new[] { vm });
+
+            var callBack = new Action<JsValue, JsValue>((err, res) =>
+            {
+                result = res?.AsString();
+            });
+
+            renderVueComponentToString.Call(null, new[] { vm, JsValue.FromObject(Engine, callBack) });
+            return result;
+        }
+    }
+}
---
+++ src/DotnetVueSsr/ScriptResource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetVueSsr
+{
+    internal class ScriptResource
+    {
+        internal static string Load()
+        {
+            var assembly = Assembly.GetAssembly(typeof(RenderContext));
+            var resources = assembly.GetManifestResourceNames();
+
+            var scripts = resources.Where(w => w.StartsWith("DotnetVueSsr.scripts") && w.EndsWith(".js"))
+                                   .OrderBy(o => o);
+
+            var scriptBuilder = new StringBuilder();
+
+            foreach (var item in scripts)
+            {
+                using var stream = assembly.GetManifestResourceStream(item);
+                var script = new StreamReader(stream).ReadToEnd();
+                scriptBuilder.AppendLine(script);
+            }
+
+            return scriptBuilder.ToString();
+        }
+    }
+}
